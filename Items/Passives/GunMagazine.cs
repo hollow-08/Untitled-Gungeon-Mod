525cc11a0ade6b8b2ec9340035f84d766fcc7467
hollow <77089306+hollow-08@users.noreply.github.com>
2023-03-11 02:29:39 -0500

    Initial commit

---
+++ Items/Passives/GunMagazine.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Alexandria.ItemAPI;
+
+namespace UGM
+{
+    public class GunMagazine : PassiveItem
+    {
+        //Call this method from the Start() method of your ETGModule extension
+        public static void Register()
+        {
+            //The name of the item
+            string itemName = "Gun Fanatic's Magazine";
+
+            //Refers to an embedded png in the project. Make sure to embed your resources! Google it
+            string resourceName = "UGM/Resources/gun_magazine";
+
+            //Create new GameObject
+            GameObject obj = new GameObject(itemName);
+
+            //Add a PassiveItem component to the object
+            var item = obj.AddComponent<GunMagazine>();
+
+            //Adds a sprite component to the object and adds your texture to the item sprite collection
+            ItemBuilder.AddSpriteToObject(itemName, resourceName, obj);
+
+            //Ammonomicon entry variables
+            string shortDesc = "Become a gun master?";
+            string longDesc = "The art of gunmanship\n\n" +
+                "This weekly magazine was once cherished by a gun fanatic. It reeks of sweat and gunpowder. Boosts all gun related stats, but reduces the player's max health, and triples their curse.";
+
+            //Adds the item to the gungeon item list, the ammonomicon, the loot table, etc.
+            //Do this after ItemBuilder.AddSpriteToObject!
+            ItemBuilder.SetupItem(item, shortDesc, longDesc, "example");
+
+            //Debuffs
+            ItemBuilder.AddPassiveStatModifier(item, PlayerStats.StatType.Health, -1, StatModifier.ModifyMethod.ADDITIVE);
+            ItemBuilder.AddPassiveStatModifier(item, PlayerStats.StatType.Curse, 3f, StatModifier.ModifyMethod.MULTIPLICATIVE);
+            //Buffs
+            ItemBuilder.AddPassiveStatModifier(item, PlayerStats.StatType.Damage, 1.5f, StatModifier.ModifyMethod.MULTIPLICATIVE);
+            ItemBuilder.AddPassiveStatModifier(item, PlayerStats.StatType.ProjectileSpeed, 1.5f, StatModifier.ModifyMethod.MULTIPLICATIVE);
+            ItemBuilder.AddPassiveStatModifier(item, PlayerStats.StatType.ReloadSpeed, 1.5f, StatModifier.ModifyMethod.MULTIPLICATIVE);
+            ItemBuilder.AddPassiveStatModifier(item, PlayerStats.StatType.Accuracy, 2f, StatModifier.ModifyMethod.MULTIPLICATIVE);
+
+
+            //Set the rarity of the item
+            item.quality = PickupObject.ItemQuality.A;
+        }
+
+        public override void Pickup(PlayerController player)
+        {
+            base.Pickup(player);
+            Module.Log($"Player picked up {DisplayName}");
+        }
+
+        public override void DisableEffect(PlayerController player)
+        {
+            Module.Log($"Player dropped or got rid of {DisplayName}");
+        }
+    }
+}
---
+++ Module.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using Alexandria;
+using Alexandria.ItemAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace UGM
+{
+    [BepInDependency(Alexandria.Alexandria.GUID)] // this mod depends on the Alexandria API: https://enter-the-gungeon.thunderstore.io/package/Alexandria/Alexandria/
+    [BepInDependency(ETGModMainBehaviour.GUID)]
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class Module : BaseUnityPlugin
+    {
+        public const string GUID = "hollow.etg.ugm";
+        public const string NAME = "Untitled Gungeon Mod";
+        public const string VERSION = "0.0.0";
+        public const string TEXT_COLOR = "#B39CF5";
+
+        public void Start()
+        {
+            ETGModMainBehaviour.WaitForGameManagerStart(GMStart);
+        }
+
+        public void GMStart(GameManager g)
+        {
+            GunMagazine.Register();
+            Log($"{NAME} v{VERSION} started successfully.", TEXT_COLOR);
+        }
+
+        public static void Log(string text, string color="#FFFFFF")
+        {
+            ETGModConsole.Log($"<color={color}>{text}</color>");
+        }
+    }
+}
